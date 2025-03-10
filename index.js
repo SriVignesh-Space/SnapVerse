27ab48444f5dcdd1684b614c344fe55e25f57a52
SriVignesh-Space <srivignesh1149@gmail.com>
2025-03-10 09:38:48 +0530

    Conversion to webp

--- index.js
+++ index.js
@@ -2,15 +2,15 @@ import {title,content} from './contents.js';
 let Title = title();
 let Content = content();
 
-const imgArr = ['img0.jpg','img1.jpg','img2.jpg','img3.jpg','img4.jpg','img5.jpg','img6.jpg','img7.jpg']
+const imgArr = ['img0.webp','img1.webp','img2.webp','img3.webp','img4.webp','img5.webp','img6.webp','img7.webp']
 
 const gradient = "linear-gradient(to top left ,rgba(0,0,0,0.7),rgba(0,0,0,0.6))"
-
+~
 
 function addImage(index){
     let img = document.querySelectorAll('.slider .image img');
     img.forEach((img)=>{
-        index = (index + 1) % imgArr.length;    
+        index = (index + 1) % imgArr.length;   
         img.src = './image/' + imgArr[index];
     })
 }
